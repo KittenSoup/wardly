b4f29d14e492b154f62dd8980697c5ed559c68e5
Callisto89 <Callisto89@users.noreply.github.com>
2018-03-31 15:02:11 +0200

    work in progress, eventlist

--- views/EventView.js
+++ views/EventView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import CustomDatePicker from '../common/CustomDatePicker'
 import BackgroundImage from '../common/BackgroundImage'
 import { getEvents } from '../services/dataHandler'
@@ -12,7 +12,7 @@ export default class EventView extends React.Component {
             events: getEvents()
         }
     }
-
+    //Kan tas bort nu
     autoCapitalize(text) {
         return text.slice(0,1).toUpperCase() + text.slice(1, text.length)
     }
@@ -35,31 +35,71 @@ export default class EventView extends React.Component {
         switch (true) {
             // Om eventet är idag:
             case now.getDate() === date.getDate():
-            timeString = 'Idag ' + date.toLocaleTimeString('sv-SE')
+            timeString = 'Today ' + date.toTimeString().substring(0, 5)
             break;
              // Om eventet är imorgon:
              case tomorrow.getDate() === date.getDate():
-             timeString = 'Imorgon ' + date.toLocaleTimeString('sv-SE')
+             timeString = 'Tomorrow ' + date.toTimeString().substring(0, 5)
              break;
             default:
-              timeString = date.toLocaleDateString('sv-SE', {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'}) + ' ' + date.toLocaleTimeString('sv-SE') + ''
+            timeString = date.toString().substring(0, 21)
           }
-        return this.autoCapitalize(timeString)
+        return timeString
     }
 
     render() {
         console.log('rendering events: ', this.state.events)
         return (
             <BackgroundImage>
-                <View style={{ marginTop: 120 }}>
-                    {this.state.events.map((event) => {
-                        return <Text key={event.id}>
-                            {this.timeFormatter(event.startTime)}
+                <View style={styles.eventListContainer}>
+                {this.state.events.map((event) => {
+                    //Varje unikt event
+                    return (
+                    <View style={styles.eventContainer}
+                    key={event.id}>
+                        <Text style={styles.blackText}>
+                        {this.timeFormatter(event.startTime)}
                         </Text>
-                    })}
-                    <CustomDatePicker />
+                        <Text style={styles.whiteText}>
+                        {event.currentPlayers.length + '/' + event.requiredPlayers}
+                        </Text>
+                    </View>
+                    )
+                })}
                 </View>
             </BackgroundImage>
         )
     }
 }
+
+// <CustomDatePicker />
+//För nya events
+
+
+const styles = StyleSheet.create({
+    eventListContainer: {
+        marginTop: 120, 
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    eventContainer: {
+        width: '80%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginBottom: '5%',
+        backgroundColor: 'rgba(92, 200, 255, 0.75)',
+        padding: 10,
+    },
+    blackText: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 20,
+    },
+    whiteText: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20,
+    }
+})
